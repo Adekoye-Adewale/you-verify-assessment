342190d161e6d1c9a63bb2b763a1c04205824798
Adekoye Adewale <adekoyeadewale@gmail.com>
2026-01-14 16:59:06 +0100

    Add navigation button to home page
    Introduced a button on the home page that navigates to the invoice popup using Next.js router. Added 'use client' directive and imported useRouter from next/navigation.

--- components/home-page/index.tsx
+++ components/home-page/index.tsx
@@ -1,8 +1,17 @@
+'use client'
+import { useRouter } from 'next/navigation';
 
 export default function HomePageComponents() {
+
+        const router = useRouter();
+
         return (
                 <div className='bg-[#F5F6FA] h-[300vh]'>
-                        index
+                        <button
+                                onClick={() => router.push('/invoice?inv=Invoice%20-%201023494%20-%202304')}
+                        >
+                                Click to open pop up
+                        </button>
                 </div>
         )
 }
