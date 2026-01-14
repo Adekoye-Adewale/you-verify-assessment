f5eed1f9a9e922cc3b725e7bb1ac3f80474fdb33
Adekoye Adewale <adekoyeadewale@gmail.com>
2026-01-14 13:56:08 +0100

    Update SideNav styles and improve menu button
    Enhanced the mobile menu button with new colors, hover effect, and cursor pointer. Adjusted SideNav container width for consistency. Made NavItem always show title text and removed unused span classes.

--- components/sideNav/index.tsx
+++ components/sideNav/index.tsx
@@ -27,14 +27,14 @@ export default function SideNav() {
         return (
                 <>
                         <div 
-                                className="fixed bottom-5 left-2.5 lg:hidden z-40 py-0.5 px-2.5 bg-[#373b47]/60 rounded-full border border-[#373b47] text-xs"
+                                className="fixed bottom-5 left-2.5 lg:hidden z-40 py-0.5 px-2.5 text-[#272930] bg-[#003EFF]/60 hover:bg-[#373b47]/60 transition-all duration-450 backdrop-blur-xs rounded-full border border-[#373b47] text-xs cursor-pointer"
                                 onClick={handleMenuToggle}
                         >
                                 <span>
                                         menu
                                 </span>
                         </div>
-                        <div className={`fixed lg:sticky top-0 left-0 py-10 px-4 md:px-6 w-full min-w-15 max-w-20 md:min-w-55 md:max-w-70 h-dvh bg-white z-10 transition-all duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
+                        <div className={`fixed lg:sticky top-0 left-0 py-10 px-4 md:px-6 w-full min-w-55 max-w-70 h-dvh bg-white z-10 transition-all duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
                                 <div className="flex flex-col gap-10">
                                         <div>
                                                 <Logo />
@@ -76,9 +76,12 @@ const NavItem = ({ icon, title, link }: NavItemProps ) => {
                 <Link 
                         href={link.href} 
                         title={link.title}
-                        className={`py-3.5 px-4 flex gap-2 items-center justify-center md:justify-start rounded-3xl border-solid border-2 ${pathname === link.href ? 'border-[#E0E0E0]' : 'border-transparent'} hover:bg-[#E0E0E0] transition-all duration-300`}
+                        className={`py-3.5 px-4 flex gap-2 items-center justify-start rounded-3xl border-solid border-2 ${pathname === link.href ? 'border-[#E0E0E0]' : 'border-transparent'} hover:bg-[#E0E0E0] transition-all duration-300`}
                 >
-                        <span title={title} className="basis-4 md:basis-6 shrink-0 size-6">
+                        <span 
+                                title={title} 
+                                // className="basis-4 md:basis-6 shrink-0 size-6"
+                        >
                                 <Image 
                                         src={icon}
                                         alt={title}
@@ -87,7 +90,7 @@ const NavItem = ({ icon, title, link }: NavItemProps ) => {
                                         height={24}
                                 />
                         </span>
-                        <span className="hidden md:block text-nowrap text-[14px]">
+                        <span className="text-nowrap text-[14px]">
                                 {title}
                         </span>
                 </Link>
