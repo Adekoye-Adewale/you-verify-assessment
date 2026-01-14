91a5e5f280d1359efc4b4aa1bc8e27996abd2134
Adekoye Adewale <adekoyeadewale@gmail.com>
2026-01-14 13:52:20 +0100

    Add type definitions for invoice popup component
    Introduces TypeScript types for sender, customer, payment information, invoice items, and various props used in the invoice popup component. These types improve type safety and code clarity for invoice-related features.

---
+++ components/invoicePopUp/types.ts
@@ -0,0 +1,81 @@
+export type Sender = {
+        img?: string;
+        name: string;
+        phoneNumber: string;
+        address: string;
+        email: string;
+}
+
+export type Customer = {
+        img?: string;
+        name: string;
+        phoneNumber: string;
+        address?: string;
+        email: string;
+}
+
+export type PaymentInformation = {
+        accountName: string;
+        accountNumber: string;
+        achRoutingNumber: string;
+        bankName: string;
+        bankAddress: string;
+}
+
+export type InvoiceItem = {
+        id?: number;
+        name: string;
+        description: string;
+        quantity: number;
+        price: number;
+}
+
+export type InvoiceProps = {
+        invoice: {
+                id: string;
+                invoiceNumber: string;
+                issueDate: string;
+                dueDate: string;
+                amount: string;
+                status: string;
+                billingCurrency: string;
+                discount: number;
+                sender: Sender;
+                customer: Customer;
+                items: InvoiceItem[];
+                paymentInformation: PaymentInformation;
+                note?: string;
+        }
+}
+
+export type PopUpHeaderProps = {
+        id: string,
+        status: string,
+}
+
+export type SenderProps = {
+        title: string
+        src?: string;
+        name: string;
+        phoneNumber: string;
+        address?: string
+        email: string
+}
+
+export type InvoicePreviewProps = {
+        sender: Sender;
+        customer: Customer;
+        invoiceNumber: string;
+        issueDate: string;
+        dueDate: string;
+        billingCurrency: string;
+        items: InvoiceItem[];
+        discount: number
+}
+
+export type TotalTableProps = {
+        subtotal: number;
+        discount: number;
+        finalTotal: number;
+}
+
