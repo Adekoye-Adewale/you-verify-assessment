57ceff6bb5632beec1eb11e71ad150335b7463e7
Adekoye Adewale <adekoyeadewale@gmail.com>
2026-01-21 13:36:06 +0100

    Configure Jest for TypeScript and improve test setup
    Added ts-jest preset and jsdom environment to Jest config, set up moduleNameMapper for path aliases, and included a setup file for @testing-library/jest-dom. Updated dependencies to include ts-jest and the latest @testing-library/react. Adjusted tsconfig to use react-jsx and include Jest types for better TypeScript support in tests.

--- jest.config.ts
+++ jest.config.ts
@@ -93,6 +93,9 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -105,6 +108,7 @@ const config: Config = {
 
   // A preset that is used as a base for Jest's configuration
   // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -140,6 +144,7 @@ const config: Config = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -152,6 +157,7 @@ const config: Config = {
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
+  testEnvironment: 'jsdom',
 
   // Adds a location field to test results
   // testLocationInResults: false,
