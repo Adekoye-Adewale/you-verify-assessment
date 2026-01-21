e5141183f51791dbdc7be7d0add347ffa47503af
Adekoye Adewale <adekoyeadewale@gmail.com>
2026-01-21 13:30:55 +0100

    Add Loading component with spinner animation
    Introduces a new Loading component that displays a centered, animated spinner for use during loading states.

---
+++ components/loading/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+        return (
+                <div className='flex items-center justify-center w-full h-full animate-pulse bg-gray-400'>
+                        <div className='border-solid border-2 border-gray-800 border-r-gray-500 rounded-full animate-spin'></div>
+                </div>
+        )
+}
