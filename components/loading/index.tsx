1480bfae8c32734b20d6bb76d4c997ffe761fa5f
Adekoye Adewale <adekoyeadewale@gmail.com>
2026-01-21 13:42:05 +0100

    Update loading spinner styles
    Changed the background color to gray-300 and added a fixed size to the spinner for improved visual consistency.

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,8 +1,8 @@
 
 export default function Loading() {
         return (
-                <div className='flex items-center justify-center w-full h-full animate-pulse bg-gray-400'>
-                        <div className='border-solid border-2 border-gray-800 border-r-gray-500 rounded-full animate-spin'></div>
+                <div className='flex items-center justify-center w-full h-full animate-pulse bg-gray-300'>
+                        <div className='size-5 border-solid border-2 border-gray-800 border-r-gray-500 rounded-full animate-spin'></div>
                 </div>
         )
 }
